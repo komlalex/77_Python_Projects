23f1c62e782eff3f082a7d43a4afb87fc19eab78
Kunyelewosi Alexander <kunalexander360@gmail.com>
2024-10-31 21:58:20 +0000

    Exercise 8

---
+++ 8_exercise.py
@@ -0,0 +1,18 @@
+"""Greatest Common Divisor (GCD) of two integers - this is the largest
+natural number that divides both of these numbers without a remainder.
+For example, for numbers 32 and 48, the greatest common divisor is 16,
+which we can write
+GCD(32, 48) = 16 .
+Implement a function called greatest_common_divisor() that determines the
+greatest common divisor of two numbers.
+Implement a function called greatest_common_divisor() that determines the
+greatest common divisor of two numbers."""
+
+
+def greatest_common_divisor(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+print(greatest_common_divisor(32, 48))
